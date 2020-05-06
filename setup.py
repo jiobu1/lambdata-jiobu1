62c921af50f21e3b781644a001fc355ceefb6bf8
Jisha Obukwelu <jisha@obukwelu.com>
2020-05-06 16:37:13 -0400

    Update Name

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     license="MIT",
-    url="https://github.com/jiobu1/lambdata-jiobu1_1",
+    url="https://github.com/jiobu1/lambdata-jiobu1",
     #keywords="",
     packages=find_packages() # ["lambdata-jiobu1_1"]
 )
