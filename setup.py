86e541ad836dff8fceb28c54cd3122b62f5acf4d
Jisha Obukwelu <jisha@obukwelu.com>
2020-05-12 23:02:48 -0400

    Update version on setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-jiobu1",  # the name that you will install via pip
-    version="1.1",
+    version="1.11",
     author="Jisha Obukwelu",
     author_email="jisha-obukwelu@lambdastudents.com",
     description="Enlarge Function",
