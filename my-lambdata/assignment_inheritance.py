10c64c15c959ab3379b793044b2388b2bb7c7929
Jisha Obukwelu <jisha@obukwelu.com>
2020-05-12 21:26:00 -0400

    Create an inheritance class that inherits directly from the DataFrame class

---
+++ my-lambdata/assignment_inheritance.py
@@ -0,0 +1,24 @@
+#assignment_oop.py
+
+from pandas import DataFrame
+
+class MyFrame(DataFrame):
+
+    def add_state_names(self):
+        """
+        Adds a column of state names to accompany a corresponding column of state abbreviation.
+        """
+        names_map = {"CA": "California", "CO": "Colorado", "CT": "Connecticut", "DC": "Washington DC", "TX": "Texas"}
+        self["name"] = self["abbrev"].map(names_map) 
+        # see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+
+if __name__ == "__main__":
+
+    #df = DataFrame({"abbrev":["CA","CO","CT","DC","TX"]})
+
+    my_frame = MyFrame({"abbrev":["CA","CO","CT","DC","TX"]})
+    print(my_frame.columns)
+    print(my_frame.head())
+
+    my_frame.add_state_names()
+    print(my_frame.head())
