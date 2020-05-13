8f00beb423b2828949449c7a6bd4c9c43eeb04cd
Jisha Obukwelu <jisha@obukwelu.com>
2020-05-12 21:24:24 -0400

    Update assigment.py from functional programming to OOP style

--- my-lambdata/assignment_oop.py
+++ my-lambdata/assignment_oop.py
@@ -1,24 +1,35 @@
 #assignment_oop.py
 
-from pandas import DataFrame
+from pandas import DataFrame #class
 
-class MyFrame(DataFrame):
+class DataProcessor ():
+    def __init__(self, my_df):
+        """
+         Params:
+            my_df (pandas.DataFrame) has a column called "abbrev" with state abbreviations.
+        """
+        self.df = my_df
 
-    def add_state_names(self):
+    def add_state_names(self): #add_state_names = function; my_df = is the object
         """
         Adds a column of state names to accompany a corresponding column of state abbreviation.
+
+        Returns:
+        copy of the orginal dataframe, with another column
         """
         names_map = {"CA": "California", "CO": "Colorado", "CT": "Connecticut", "DC": "Washington DC", "TX": "Texas"}
-        self["name"] = self["abbrev"].map(names_map) 
-        # see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+        self.df["name"] = self.df["abbrev"].map(names_map)
+        # see:
+        # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+        return self.df
+
 
 if __name__ == "__main__":
+    df = DataFrame({"abbrev":["CA","CO","CT","DC","TX"]})
 
-    #df = DataFrame({"abbrev":["CA","CO","CT","DC","TX"]})
+    processor = DataProcessor(df)
+    print(processor.df.head())
+    
+    processor.add_state_names()
+    print(processor.df.head())
 
-    my_frame = MyFrame({"abbrev":["CA","CO","CT","DC","TX"]})
-    print(my_frame.columns)
-    print(my_frame.head())
-
-    my_frame.add_state_names()
-    print(my_frame.head())
